dde6fa70815cc5fa1886d1ae32c2205f356bb9ab
Bijon Miles <bijonmiles01@gmail.com>
2018-10-12 13:35:59 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'hello_world', to: 'static#hello_world'
+
+  # resourses :hello_worlds, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
